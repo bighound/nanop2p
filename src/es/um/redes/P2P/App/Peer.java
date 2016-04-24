ceca7fe882e617972c4a058bd4fb7d6fee38aeec
Jorge Gallego Madrid <jorge@localhost.localdomain>
2016-04-24 14:08:18 +0200

    Intentando leer los mensajes

--- src/es/um/redes/P2P/App/Peer.java
+++ src/es/um/redes/P2P/App/Peer.java
@@ -2,6 +2,8 @@ package es.um.redes.P2P.App;
 
 
 import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.Scanner;
 
 import es.um.redes.P2P.PeerTracker.Client.Reporter;
@@ -25,23 +27,34 @@ public class Peer {
 		
 		FileInfo[] localFile = FileInfo.loadFilesFromFolder(peerSharedFolder);
 		
-
+		System.out.println("Comandos: addseeds, queryfiles, download, getseeds, removeseed, exit");
 		while (continua) {
 			Scanner scan = new Scanner(System.in);
 			String entrada = scan.nextLine();
 			String [] arg = entrada.split("\\s+");
 			
-			//Add seeds, query files, download, getseeds, removeseed
+			//Add seeds, query files, download, getseeds, removeseed, exit
 			switch (arg[0]) {
 			case "addseeds":
 				System.out.println((client.sendMsg(Message.OP_ADD_SEED, localFile)).toString());
 				break;
 			case "queryfiles":
-				System.out.println((client.sendMsg(Message.OP_QUERY_FILES, localFile)).toString());
+				Message mensaje = client.sendMsg(Message.OP_QUERY_FILES, localFile);
+				System.out.println(mensaje.toString());			
+				break;
+			case "download":
+				System.out.println("Descargando...");
 				break;
 			case "getseeds":
+				if (arg[1] != null){
+					System.out.println("Comando utilizado correctamente con el argumento" + arg[1]);
+				}
 				System.out.println((client.sendMsg(Message.OP_GET_SEEDS, localFile)).toString());
 				break;
+			case "removeseeds":
+				System.out.println((client.sendMsg(Message.OP_REMOVE_SEED, localFile)).toString());
+				break;	
+			
 			case "cliente":
 				Socket socketCliente = new Socket("127.0.0.1",4450);
 				//socketCliente.connect(null);
@@ -62,6 +75,8 @@ public class Peer {
 				System.out.println("Fin del proceso del peer");
 				break;
 			default:
+				System.out.println("Comando incorrecto");
+				System.out.println("Comandos: addseeds, queryfiles, download, getseeds, removeseed, exit");
 				break;
 			}
 		}
